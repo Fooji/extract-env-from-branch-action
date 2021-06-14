f723036bf692bccfe25b6f3d38698f8a9e28edfe
William Coates <william@fooji.com>
2021-06-14 15:02:28 +0100

    add debug

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const core = require('@actions/core');
 try {
   const branch = core.getInput('branchRef');
-  const [stage, env] = branch.replace('refs/heads/', '').split('-')
+  const [stage, env] = branch.replace('refs/heads/', '').split('-');
   core.exportVariable('FOOJI_STAGE', stage);
   core.exportVariable('FOOJI_ENV', env);
   let domain
   switch (stage) {
     case 'dev':
-      domain = 'fooji.dev';
+      domain = `${env}.fooji.dev`;
       break;
     case 'staging':
-      domain = 'fooji.qa';
+      domain = `${env}.fooji.qa`;
       break;
     case 'production':
-      domain = 'fooji.com';
+      domain = `${env}.fooji.com`;
       break;
     default:
-      throw Error(`Unknown stage ${stage}!`)
+      throw Error(`Unknown stage ${stage}!`);
   }
+  // TODO: Do we need this?
   core.exportVariable('FOOJI_DOMAIN', domain);
+  console.log(`FOOJI_STAGE=${stage}, FOOJI_ENV=${env}, FOOJI_DOMAIN=${domain}`);
 } catch (error) {
   core.setFailed(error.message);
 }
