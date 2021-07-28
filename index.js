067ef799b2d4462ee603c63002ca9128358fcde2
William Coates <william@fooji.com>
2021-07-28 10:15:25 +0100

    feat: initial version (#1)

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+try {
+  const branch = core.getInput('branchRef');
+  const [stage, env] = branch.replace('refs/heads/', '').split('-');
+  core.exportVariable('FOOJI_STAGE', stage);
+  core.exportVariable('FOOJI_ENV', env);
+  core.exportVariable('FOOJI_PROFILE', `${env}-${stage}`);
+  let domain
+  switch (stage) {
+    case 'dev':
+      domain = `${env}.fooji.dev`;
+      break;
+    case 'staging':
+      domain = `${env}.fooji.qa`;
+      break;
+    case 'production':
+      domain = `${env}.fooji.com`;
+      break;
+    default:
+      throw Error(`Unknown stage ${stage}!`);
+  }
+  core.exportVariable('FOOJI_DOMAIN', domain);
+  console.log(`FOOJI_STAGE=${stage}, FOOJI_ENV=${env}, FOOJI_DOMAIN=${domain}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
